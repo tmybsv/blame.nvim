052a09ef8f2a4704e9c2d020eeef46c717cb83ca
timofey basuev <echiauta@gmail.com>
2025-08-15 10:05:22 +0500

    fix: add more margin for blame line

--- lua/blame.lua
+++ lua/blame.lua
@@ -41,7 +41,7 @@ local function show()
 		end
 	end
 	if not atime then return end
-	local msg = string.format("%s %s %s", author, reltime(atime), summary)
+	local msg = string.format("  %s %s %s", author, reltime(atime), summary)
 	clear()
 	vim.api.nvim_buf_set_extmark(0, ns, lnum - 1, 0, {
 		virt_text = { { msg, "BlameVirtText" } },
